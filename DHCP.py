f96756c010debd210fb492c9981a27b82786c34c
fundamentaleq <bloodspain@gmail.com>
2017-04-10 19:52:20 +0530

    Added DHCP offer

--- DHCP.py
+++ DHCP.py
@@ -21,3 +21,33 @@ def DhcpDiscover(MacAddress) :
 
     package = OP + HTYPE + HLEN + HOPS + XID + SECS + FLAGS + CIADDR +YIADDR + SIADDR + GIADDR + CHADDR1 + CHADDR2 + CHADDR3 + CHADDR4 + CHADDR5 + MagicCookie + DHCPOptions1 + End
     return package
+
+
+def DhcpOffer(ClientMacAddress,TransactionId,ServerIp,OfferedIp,SubnetMask) :
+    OP = bytes([0x02])
+    HTYPE = bytes([0x01])
+    HLEN = bytes([0x06])
+    HOPS = bytes([0x00])
+    XID = TransactionId
+    SECS = bytes([0x00, 0x00])
+    FLAGS = bytes([0x00, 0x00])
+    CIADDR = bytes([0x00, 0x00, 0x00, 0x00])
+    YIADDR = bytes(OfferedIp)
+    SIADDR = bytes(ServerIp)
+    GIADDR = bytes([0x00, 0x00, 0x00, 0x00])
+    CHADDR1 = ClientMacAddress
+    CHADDR2 = bytes([0x00, 0x00])
+    CHADDR3 = bytes([0x00, 0x00, 0x00, 0x00])
+    CHADDR4 = bytes([0x00, 0x00, 0x00, 0x00])
+    CHADDR5 = bytes(192)
+    Magiccookie = bytes([0x63, 0x82, 0x53, 0x63])
+    DHCPOptions1 = bytes([53 , 1 , 2]) # DHCP Offer
+    DHCPOptions2 = bytes([1 , 4 , SubnetMask[0], SubnetMask[1], SubnetMask[2], SubnetMask[3] ]) # SubnetMask
+    DHCPOptions3 = bytes([3 , 4 , ServerIp[0], ServerIp[1], ServerIp[2], ServerIp[3] ]) #192.168.1.1 router
+    DHCPOptions4 = bytes([51 , 4 , 0x00, 0x01, 0x51, 0x80]) #86400s(1 day) IP address lease time
+    DHCPOptions3 = bytes([3 , 4 , ServerIp[0], ServerIp[1], ServerIp[2], ServerIp[3] ]) # DHCP Server
+    End = bytes([0xff])
+
+    package = OP + HTYPE + HLEN + HOPS + XID + SECS + FLAGS + CIADDR +YIADDR + SIADDR + GIADDR + CHADDR1 + CHADDR2 + CHADDR3 + CHADDR4 + CHADDR5 + Magiccookie + DHCPOptions1 + DHCPOptions2 + DHCPOptions3 + DHCPOptions4 + DHCPOptions5 + End
+
+    return package
