d0f8d46eeecbe72d6fa143c82827d3c1d25acc60
fundamentaleq <bloodspain@gmail.com>
2017-04-11 13:37:32 +0530

    Added DHCPrequest

--- DHCP.py
+++ DHCP.py
@@ -51,3 +51,30 @@ def DhcpOffer(ClientMacAddress,TransactionId,ServerIp,OfferedIp,SubnetMask) :
     package = OP + HTYPE + HLEN + HOPS + XID + SECS + FLAGS + CIADDR +YIADDR + SIADDR + GIADDR + CHADDR1 + CHADDR2 + CHADDR3 + CHADDR4 + CHADDR5 + Magiccookie + DHCPOptions1 + DHCPOptions2 + DHCPOptions3 + DHCPOptions4 + DHCPOptions5 + End
 
     return package
+
+def DhcpRequest(MacAddress,TransactionId,ServerIp,RequestedIp):
+    OP = b'\x01'     #Message type: Boot Request (1)
+    HTYPE = b'\x01'  #Hardware type: Ethernet
+    HLEN = b'\x06'   #Hardware address length: 6
+    HOPS = b'\x00'   #Hops: 0
+    XID = TransactionId   #Transaction ID
+    SECS = b'\x00\x00'   	    #Seconds elapsed: 0
+    FLAGS = b'\x80\x00'	    #Bootp flags: 0x8000 (Broadcast) + reserved flags
+    CIADDR = b'\x00\x00\x00\x00'   #Client IP address: 0.0.0.0
+    YIADDR = b'\x00\x00\x00\x00'   #Your (client) IP address: 0.0.0.0
+    SIADDR = bytes(ServerIp)   #Next server IP address: 0.0.0.0
+    GIADDR = b'\x00\x00\x00\x00'   #Relay agent IP address: 0.0.0.0
+    CHADDR1 = MacAddress
+    CHADDR2 = b'\x00\x00'
+    CHADDR3 = b'\x00\x00\x00\x00'
+    CHADDR4 = b'\x00\x00\x00\x00'
+    CHADDR5 = bytes(192)               	     #Client hardware address padding: 00000000000000000000
+    MagicCookie = b'\x63\x82\x53\x63'  	     #Magic cookie: DHCP
+    DHCPOptions1 = bytes([53 , 1 , 3])     	     #Option: (t=53,l=1) DHCP Message Type = DHCP Request
+    DHCPOptions2 = bytes([50 , 4 , RequestedIp[0], RequestedIp[1], RequestedIp[2], RequestedIp[3]])   #Option: (t=50,l=4) Requested IP Adress
+    DHCPOptions3 = bytes([54 , 4 , ServerIp[0], ServerIp[1], ServerIp[2], ServerIp[3]])   #Option: (t=54,l=4) DHCP Server Identifier
+    End = b'\xff'   #End option
+
+    package = OP + HTYPE + HLEN + HOPS + XID + SECS + FLAGS + CIADDR +YIADDR + SIADDR + GIADDR + CHADDR1 + CHADDR2 + CHADDR3 + CHADDR4 + CHADDR5 + MagicCookie + DHCPOptions1 + DHCPOptions2 +  DHCPOptions3 + End
+
+    return package
