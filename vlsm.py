46076c7643b9a65d81923eedcf025c8c0f9a4789
Kritika <kritikagr12@gmail.com>
2017-04-13 19:28:12 +0530

    added function to reurn ip addresses

--- vlsm.py
+++ vlsm.py
@@ -2,6 +2,7 @@ from math import pow, ceil, log
 from sys import argv
 import re
 
+info = {}
 
 def min_pow2(x):  # how many bits do we need to borrow
     z = log(x, 2)  # to cover number of hosts
@@ -62,6 +63,27 @@ def getnextaddr(ipaddr, nmask):
     return ipaddr
 
 
+def getnextip(ipaddr):
+    for i in range(4):
+        if ipaddr[3 - i] == 255:
+            ipaddr[3 - i] = 0
+            if ipaddr[3 - i - 1] != 255:
+                ipaddr[3 - i - 1] += 1
+                break
+        else:
+            ipaddr[3 - i] += 1
+            break
+    return ipaddr
+
+
+def lessthan(ip1,ip2):#if ip1 less than or equal to ip2
+    for i in range(0,4):
+        if(ip1[i] <= ip2[i]):
+            continue
+        else:
+            return False
+    return True
+
 def norm(ipaddr):
     addr = ipaddr[:]
     for i in range(len(addr)):
@@ -76,15 +98,15 @@ def vlsm(ipaddr, hosts):
     for x in range(len(hosts)):
         bits = min_pow2(hosts[x][0] + 2)
         ipaddr = getnet(ipaddr, getmask(int(32 - bits)))
-
-        print " SUBNET: %d NEEDED: %3d (%3d %% of) ALLOCATED %4d ADDRESS: %15s :: %15s - %-15s :: %15s MASK: %d (%15s)" % \
-              (x + 1,
+        #" SUBNET: %d NEEDED: %3d (%3d %% of) ALLOCATED %4d ADDRESS: %15s :: %15s - %-15s :: %15s MASK: %d (%15s)" % \
+        info[str(hosts[x][1])] = (x + 1,
                hosts[x][0],
                (hosts[x][0] * 100) / (int(pow(2, bits)) - 2),
                int(pow(2, bits)) - 2,
-               norm(ipaddr),
-               norm(getfirst(ipaddr)),
-               norm(getlast(ipaddr, getmask(int(32 - bits)))),
+               ipaddr,
+               getfirst(ipaddr),
+               getfirst(ipaddr),
+               getlast(ipaddr, getmask(int(32 - bits))),
                norm(getbcast(ipaddr, getmask(int(32 - bits)))),
                32 - bits,
                norm(getmask(int(32 - bits))))
@@ -92,50 +114,95 @@ def vlsm(ipaddr, hosts):
         need += hosts[x][0]
         allc += int(pow(2, bits)) - 2
         ipaddr = getnextaddr(ipaddr, getmask(int(32 - bits)))
+        
 
-args=[]
-with open('subnets.conf', 'r') as f:
-    for line in f:
-        args.append(line)
+def getip(mac): #returns ip for client and dns server
+    args=[]
+    with open('subnets.conf', 'r') as f:
+        for line in f:
+            args.append(line)
 
-ip = args[0].split("/")[0].split(".")   # 192.168.1.0/24 2 8 22 54  -> list of str ['192','168','1','0']
-cidr = int(args[0].split("/")[1])       # 192.168.1.0/24 2 8 22 54  -> str 24
-mask = getmask(cidr)                    #                       24  -> list of int [255,255,255,0]
-total_hosts = 0
-n = int(args[1])
-labs={}
+    ip = args[0].split("/")[0].split(".")   # 192.168.1.0/24 2 8 22 54  -> list of str ['192','168','1','0']
+    cidr = int(args[0].split("/")[1])       # 192.168.1.0/24 2 8 22 54  -> str 24
+    mask = getmask(cidr)                    #                       24  -> list of int [255,255,255,0]
+    total_hosts = 0
+    n = int(args[1])
+    labs={}
 
-for i in range(2,2+n):
-    labs[str(str(args[i]).split(':')[0])]= int(str(args[i]).split(':')[1])
-    total_hosts+= int(str(args[i]).split(':')[1])
+    for i in range(2,2+n):
+        labs[str(str(args[i]).split(':')[0])]= int(str(args[i]).split(':')[1])
+        total_hosts+= int(str(args[i]).split(':')[1])
 
-t = [(labs[i],i) for i in labs]
-t = sorted(t,reverse=True)
+    for x in range(len(ip)):  # list of str ['192','168','1','0'] ->
+        ip[x] = int(ip[x])  # list of int [192,168,1,0]
 
-for x in range(len(ip)):  # list of str ['192','168','1','0'] ->
-    ip[x] = int(ip[x])  # list of int [192,168,1,0]
+    if total_hosts > (pow(2, 32 - cidr) - 2):
+        Error = "Too many hosts"
 
-if total_hosts > (pow(2, 32 - cidr) - 2):
-    exit("ERROR: Too many hosts")
+    if (pow(2, 32 - cidr) - 2 - total_hosts) > 0:
+        labs['others'] = pow(2, 32 - cidr) - 2 - total_hosts
+        no_others = False
+    else :
+        no_others = True
+        DHCP_IP = None
+        Error = "Ip cant be assigned to DHCP server"
 
-masks={}
-for i in range(2+n,len(args)):
-    masks[str(args[i]).split(' ')[0]] = str(args[i]).split(' ')[1]
-print masks
-need = 0
-allc = 0
+    t = [(labs[i],i) for i in labs]
+    t = sorted(t,reverse=True)
+    need = 0
+    allc = 0
 
-print
-vlsm(getnet(ip, mask), t)
-print
+    vlsm(getnet(ip, mask), t)
 
-print
-print " STATISTICS"
-print " =========="
-print " Major Network: %s/%d" % (norm(getnet(ip, mask)), cidr)
-print " Available IP addresses in major network: %d" % (pow(2, 32 - cidr) - 2)
-print " Number of IP addresses needed: ", need
-print " Available IP addresses in allocated subnets: ", allc
-print " About %d%% of available major network address space is used" % (
-    ((allc + (n * 2)) * 100) / (pow(2, 32 - cidr)))
-print " About %d%% of subnetted network address space is used" % (need * 100 / allc)
+    macs={}
+    for i in range(2+n,len(args)):
+        macs[str(args[i]).split(' ')[0]] = str(args[i]).split(' ')[1]
+
+    if(macs.has_key(mac)):
+        l = macs.get(mac)
+    else:
+        if(no_others):
+            Error = "IP cant be assigned"
+        else:
+            l = 'others'
+            Error = None
+
+    if Error:
+        return 0,Error
+    else:
+        if(l != 'others'):
+            if lessthan(getnextip(info[l][6]), info[l][7]):
+                client_ip = info[l][6] = getnextip(info[l][6])
+            else:
+                client_ip = None
+
+            dhcp_ip = getnextip(info['others'][5])
+            mask = info[l][10]
+            gateway_ip = dns_ip = info[l][5]
+        else:
+            dhcp_ip = getnextip(info['others'][5])
+            if lessthan(getnextip(info[l][6]), info[l][7]):
+                if(getnextip(info[l][6])!= dhcp_ip):
+                    client_ip = info[l][6] = getnextip(info[l][6])
+                else:
+                    if lessthan(dhcp_ip, info[l][7]):
+                        client_ip = info[l][6] = getnextip(info[l][6])
+                    else :
+                        client_ip = None
+                if client_ip:
+                    mask = info[l][10]
+                    gateway_ip = dns_ip = info[l][5]
+            else:
+                client_ip = None
+        if client_ip:
+            client_ip = norm(client_ip)
+            dhcp_ip = norm(dhcp_ip)
+            gateway_ip = norm(gateway_ip)
+            dns_ip = norm(dns_ip)
+        else:
+            Error = "IP cant be assigned"
+
+    if Error :
+        return 0,Error
+    else:
+        return 1,{'ClientIP':client_ip ,'mask':mask , 'DHCPIP':dhcp_ip , 'GatewayIP':gateway_ip , 'DNSIP':dns_ip }
