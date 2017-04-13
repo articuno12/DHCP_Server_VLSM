9e8b3860d9496a534f41cb0aea2f1b4e8f3c4c46
fundamentaleq <bloodspain@gmail.com>
2017-04-13 17:55:32 +0530

    Added DHCP ack and print results in client

--- client.py
+++ client.py
@@ -60,6 +60,10 @@ while not OfferRecieved :
         if TransactionId != TransactionId_recieved :
             print("Recieved Unknow message")
             continue
+        MsgOptions = DHCP.ExtractOption(data)
+        if (not hex(53) in MsgOptions) or MsgOptions[hex(53)] != hex(2) :
+            print("Msg type not offer")
+            continue
         OfferedIp = DHCP.bytestring2hex(data[16:20],4)
         ServerIp = DHCP.bytestring2hex(data[20:24],4)
         OfferRecieved = True
@@ -74,3 +78,57 @@ package = DHCP.DhcpRequest(MacAddress,TransactionId,ServerIp,OfferedIp)
 sock.sendto(package,dest)
 
 # DHCP Acknowledgement - Server
+AckRecieved = False
+while not AckRecieved :
+    try :
+        sock.settimeout(5)
+        data ,address = sock.recvfrom(MAX_BYTES)
+        TransactionId_recieved = data[4:8]
+        TransactionId_recieved = DHCP.bytestring2hex(TransactionId_recieved,4)
+        if TransactionId != TransactionId_recieved :
+            print("Recieved Unknow message")
+            continue
+        MsgOptions = DHCP.ExtractOption(data)
+        if (not hex(53) in MsgOptions) or MsgOptions[hex(53)] != hex(5) :
+            print("Msg type not Ack")
+            continue
+        MyIp = DHCP.bytestring2hex(data[16:20],4)
+        SubnetMask = MsgOptions[hex(1)]
+        GateWayIp = MsgOptions[hex(3)]
+        LeaseTime =  MsgOptions[hex(51)]
+        DNSIp = MsgOptions[hex(6)]
+        AckRecieved = True
+
+    except socket.timeout :
+        sock.sendto(package, dest)
+
+
+MyIp = [ int(MyIp[i],16) for i in range(4)]
+SubnetMask = [ int(SubnetMask[i],16) for i in range(4)]
+GateWayIp = [ int(GateWayIp[i],16) for i in range(4)]
+DNSIp = [ int(DNSIp[i],16) for i in range(4)]
+NetworkIp = [ MyIp[i] & SubnetMask[i] for i in range(4)]
+Cidr = sum([bin(SubnetMask[i]).count('1') for i in range(4)])
+
+def getbcast(ipaddr, nmask):  # Get broadcast address from ip and mask
+    net = [0 for i in range(4)]
+    for i in range(4):
+        net[i] = int(ipaddr[i]) | 255 - int(nmask[i])  # octet or wildcard mask
+    return net
+
+BroadcastIp  = getbcast(NetworkIp,SubnetMask)
+
+MyIp = '.'.join([str(MyIp[i]) for i in range(4)])
+NetworkIp = '.'.join([str(NetworkIp[i]) for i in range(4)])
+BroadcastIp = '.'.join([str(BroadcastIp[i]) for i in range(4)])
+GateWayIp = '.'.join([str(GateWayIp[i]) for i in range(4)])
+DNSIp = '.'.join([str(DNSIp[i]) for i in range(4)])
+
+
+MyIp = MyIp + '/' + str(Cidr)
+
+print MyIp
+print NetworkIp
+print BroadcastIp
+print GateWayIp
+print DNSIp
